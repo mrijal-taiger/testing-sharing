0bc44e048f27fe70f7a9e2787c558a5f0ac6c7a3
Muhammad Rizki Rijal <muhammad.rijal@taiger.com>
2021-12-22 17:14:23 +0800

    add orderGetTotal test

--- src/test/java/com/example/testco/entity/OrderTest.java
+++ src/test/java/com/example/testco/entity/OrderTest.java
@@ -15,4 +15,15 @@ public class OrderTest {
 
     assertEquals(2, order.lineItemsCount());
   }
+
+  @Test
+  public void testGetTotalOrder() throws Exception {
+    Product product1 = new Product(1, "Carrot", 10);
+    Product product2 = new Product(2, "Onion", 5);
+    Order order = Order.create();
+    order.add(product1, 1);
+    order.add(product2, 3);
+
+    assertEquals(25, order.getTotal());
+  }
 }
