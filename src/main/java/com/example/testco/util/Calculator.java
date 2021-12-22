e635ff98c946fa5a261ecf775d12c7cb98690f69
Muhammad Rizki Rijal <muhammad.rijal@taiger.com>
2021-12-22 15:11:12 +0800

    add Calculator.sum method implementation

--- src/main/java/com/example/testco/util/Calculator.java
+++ src/main/java/com/example/testco/util/Calculator.java
@@ -1,5 +1,11 @@
 package com.example.testco.util;
 
 public class Calculator {
-  
+  public int sum(int a, int... b) {
+    int result = a;
+    for (int number : b) {
+      result += number;
+    }
+    return result;
+  }
 }
