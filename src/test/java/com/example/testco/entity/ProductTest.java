7bfa8bbc2e8349f057243c01a9e13408a6f93c78
Muhammad Rizki Rijal <muhammad.rijal@taiger.com>
2021-12-23 07:47:32 +0800

    assert product validation exception message

--- src/test/java/com/example/testco/entity/ProductTest.java
+++ src/test/java/com/example/testco/entity/ProductTest.java
@@ -3,9 +3,17 @@ package com.example.testco.entity;
 import static org.junit.Assert.assertEquals;
 
 import com.example.testco.exception.ProductValidationException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ProductTest {
+  @Rule
+  public ExpectedException expectedEx = ExpectedException.none();
+
   @Test(expected = ProductValidationException.class)
   public void when_providedPriceLowerThanZero_throwsProductException() throws Exception {
     new Product("Lettuce", -1);
@@ -16,6 +24,16 @@ public class ProductTest {
     new Product("Lettuce", 0);
   }
 
+  @Test
+  public void errorMessageIsCorrect() throws Exception {
+    String expectedErrorMessage = "Price should be greater than 0";
+
+    expectedEx.expect(ProductValidationException.class);
+    expectedEx.expectMessage(expectedErrorMessage);
+    new Product("Lettuce", 0);
+  }
+
+  @Test
   public void getPrice_returns_correctPrice() throws Exception {
     Product product = new Product("Lettuce", 100);
     assertEquals(100, product.getPrice());
